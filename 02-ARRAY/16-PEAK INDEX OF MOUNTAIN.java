afdb1a556142df458f4fbad544089ae32ea6499f
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-09-10 19:46:58 +0530

    Update 16-PEAK INDEX OF MOUNTAIN.java

--- 02-ARRAY/16-PEAK INDEX OF MOUNTAIN.java
+++ 02-ARRAY/16-PEAK INDEX OF MOUNTAIN.java
@@ -1,5 +1,5 @@
 https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
-
+https://leetcode.com/problems/find-peak-element/description/
 You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
 Return the index of the peak element.
 Your task is to solve it in O(log(n)) time complexity.
