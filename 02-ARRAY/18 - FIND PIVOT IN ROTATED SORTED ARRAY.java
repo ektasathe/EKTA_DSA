700cd440270649fe8b8fab0a99e829cad5962c91
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-09-16 08:45:48 +0530

    Update 18 - FIND PIVOT IN ROTATED SORTED ARRAY.java

--- 02-ARRAY/18 - FIND PIVOT IN ROTATED SORTED ARRAY.java
+++ 02-ARRAY/18 - FIND PIVOT IN ROTATED SORTED ARRAY.java
@@ -1,10 +1,34 @@
+CASE 1 : SUPPOSE MID IS AT INDEX 4  || if (mid < end && arr[mid] > arr[mid + 1])
+  0  1  2  3  4  5  6  7  8
+[ 5, 6, 7, 8, 9, 1, 2, 3, 4 ]
+  S           M  M+1       E
 
+CASE 2 : SUPPOSE MID IS AT INDEX 5  ||  if (mid > start && arr[mid] < arr[mid - 1]
+  0  1  2  3  4  5  6  7  8
+[ 5, 6, 7, 8, 9, 1, 2, 3, 4 ]
+  S          M-1 M        E
+
+CASE 3 : EXCLUED CASE THAT MID IS AT INDEX 4 OR 5 CAUSE ALREADY COVERED ABOVE , 
+  || if (arr[mid] <= arr[start])  || NOW WE WANT PIVOT SO EXCLUDE LEFT SIDE AS LARGEST ELEMENT WILL LIE BETWEEN  ARR[START] AND ARR[MID-1]
+  0  1  2  3  4  5  6  7  8
+[ 5, 6, 7, 8, 9, 1, 2, 3, 4 ]
+  S                 M     E
+
+CASE 4 : EXCLUED CASE THAT MID IS AT INDEX 4 OR 5 CAUSE ALREADY COVERED ABOVE 
+  || arr[mid] >= arr[end] || WE WANT PIVOT SO EXCLUDE LEFT SIDE AS LARGEST ELEMENT WILL LIE BETWEEN ARR[MID+1] AND ARR[END]
+  0  1  2  3  4  5  6  7  8
+[ 5, 6, 7, 8, 9, 1, 2, 3, 4 ]
+  S     M                 E
+  
+
+/ WHEN YOU USE STATIC INSIDE A CLASS THEN ALL THE ELEMENTS THAT ARE DECLARES IN IT SHOULD BE USED INSIDE THAT BLOCK ITSELF /
+  
 import java.util.*;
 class BinarySearch {
   
     public static void main(String args[]) 
   {
-    int arr[] = { 3,4,8,9,1,2 };
+    int arr[] = { 3,4,8,9,1,2,3};
     int n = arr.length;
     //int K = 4;
      System.out.println(findpivotindex(arr));
