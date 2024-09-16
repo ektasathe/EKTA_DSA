cd03bf7e1280c75d3cb01c299d1e8281915e3c65
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-09-16 08:07:41 +0530

    Update 18 - FIND PIVOT IN ROTATED SORTED ARRAY.java

--- 02-ARRAY/18 - FIND PIVOT IN ROTATED SORTED ARRAY.java
+++ 02-ARRAY/18 - FIND PIVOT IN ROTATED SORTED ARRAY.java
@@ -1,35 +1,40 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+
 import java.util.*;
 class BinarySearch {
   
     public static void main(String args[]) 
   {
-    BinarySearch ob = new BinarySearch();
+    //BinarySearch ob = new BinarySearch(); //creation of function object
     int arr[] = { 3,4,8,9,1,2 };
     int n = arr.length;
-    int K = 4;
-    int result = ob.binarySearch(arr); //at last only array of ingle digit would be present that would be returned  and that would contain only pivot index
+    //int K = 4;
+   /* int result = ob.findpivot(arr); //at last only array of ingle digit would be present that would be returned  and that would contain only pivot index
     if (result == -1)
       System.out.println("Not found");
     else
-      System.out.println("Element found at index " + result);
+      System.out.println("Element found at index " + result); */
+     System.out.println(findpivot((arr));
   }
   
-  int binarySearch(int arr[]) {
+ // int binarySearch(int arr[]) { #if yo write this then you need to create object of the function so use static
+  static findpivot(int arr[]) {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
             // 4 cases over here
-            if (mid < end && arr[mid] > arr[mid + 1]) {
+          //case 1 :mid index should be smaller than end  and if arr[mid] > arr[mid+1]
+            if (mid < end && arr[mid] > arr[mid + 1]) {  
                 return mid;
             }
+          //case 2 : mid index should be greater than start and arr[mid] < arr[mid-1]
             if (mid > start && arr[mid] < arr[mid - 1]) {
                 return mid-1;
             }
+          //case 3 :arr[mid] <=arr[start]
             if (arr[mid] <= arr[start]) {
                 end = mid - 1;
+          //case 4 : arrarr[mid] >= arr;end]
             } else {
                 start = mid + 1;
             }
