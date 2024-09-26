d954ca09c9b0a04bb38f5ff5a13a87ccace75bdd
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-09-26 08:56:52 +0530

    Update 02-BINARY _SEARCH_DESC.java

--- 03 BINARY SEARCH /02-BINARY _SEARCH_DESC.java
+++ 03 BINARY SEARCH /02-BINARY _SEARCH_DESC.java
@@ -10,8 +10,8 @@ STEP 4 :  IF(TARGET == MID ELEMENT) THEN RETURN 1 OE INDEX, ELSE RETURN -1
 n an array arr[] sorted in ascending order of size N and an integer K. Check if K is present in the array or not.
 
 N = 7, target = 6
-arr[] = {16,13,11,9,4,2}
-Output: 1 => AS 6 IS PRESENT IN ARRAY
+arr[] = {16,13,11,6,4,2}
+Output: 3 => AS 6 IS PRESENT IN ARRAY
 
 class Solution {
     static int searchInSorted(int arr[], int N, int target) {
@@ -22,17 +22,17 @@ class Solution {
           // int mid = ( start + end ) / 2     ====> here are chances that mid value can exceed the max integet input digits
         //  max val int cans tore = 10^9 . if start and end values are very high then then mid value can overflow , to avoid it use below
             int mid = start + (end - start)/2 ;   ===> so use this 
+            if 
+            {
+                return mid ;//return index of element if element is found
+            }
             if( target < arr[mid])
             {
                 start = mid + 1 ;
             }
-            else if( target > arr[mid] )
+            else //if( target > arr[mid] )
             {
                 end = mid - 1;
-            }
-            else 
-            {
-                return 1 ;
             }
         }
         return -1 ;
