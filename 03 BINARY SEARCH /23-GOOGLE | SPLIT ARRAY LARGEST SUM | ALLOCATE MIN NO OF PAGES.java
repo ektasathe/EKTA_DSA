af8925ea7d8c018c9e424bcc77d6392d3adcc710
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-09-29 20:51:41 +0530

    Update 23-GOOGLE | SPLIT ARRAY LARGEST SUM | ALLOCATE MIN NO OF PAGES.java

--- 03 BINARY SEARCH /23-GOOGLE | SPLIT ARRAY LARGEST SUM | ALLOCATE MIN NO OF PAGES.java
+++ 03 BINARY SEARCH /23-GOOGLE | SPLIT ARRAY LARGEST SUM | ALLOCATE MIN NO OF PAGES.java
@@ -1,5 +1,5 @@
 https://leetcode.com/problems/split-array-largest-sum/
-
+https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1 
 
 EXAMPLE
 Input: nums = [7,2,5,10,8], k = 2  |  Output: 18
@@ -51,25 +51,34 @@ end = 32 -> sum of all elements of [7, 2, 5, 10, 8]
   
 ----------------------------------------------------------------------------------------------------------------
   
-  
+ 
 class Solution {
-    public int splitArray(int[] nums, int k) {
+    // Function to find minimum number of pages.
+    public static long findPages(int n, int[] nums, int m) {
+        // Your code here
         int start = 0 ;//initial start we want max element of entire array
         int end = 0 ;// we want total sum of each element
-        //At start store the max element of array //At end strore the sum of entire array
+
+        //At start store the max element of array
+        //At end strore the sum of entire array
         for (int i=0; i<nums.length ; i++)
         {
             start = Math.max(start,nums[i]);
             end = end + nums[i];
+        }
+        //if no of books are 4 and students are more than 4 , then question condition is not satisfied, 
+        if(m > nums.length)
+        {
+            return -1;
         }
         while(start < end ) //here <= will not come because we want cause we want loop to break when start = end 
         {
             int mid = start + (end - start)/2;
             int sum = 0;
             int pieces = 1;
-            for(int num : nums){
-                if(sum+num <= mid)
-                {
+            for(int num : nums)
+            {
+                if(sum+num <= mid){
                     sum = sum + num;          
                 }
                 else{
@@ -77,13 +86,19 @@ class Solution {
                     pieces ++; 
                 }
             }  
-            if(pieces <= k) { //search in left side of mid element
+            //search in left side of mid element
+            if(pieces <= m) {
                 end = mid;       
             }
-            else {//searcing in right side of mid element 
+            //searcing in right side of mid element
+            else  {
                  start = mid+1;               
             }
         }
         return end;   
     }
-}
+};
+
+
+
+
