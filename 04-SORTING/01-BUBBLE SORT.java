347d0905b928068a5b56bad66b8a970fae8fb24d
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-09-30 22:19:39 +0530

    array sort

--- 04-SORTING/01-BUBBLE SORT.java
+++ 04-SORTING/01-BUBBLE SORT.java
@@ -65,3 +65,33 @@ class bubble
         }
     }
 }
+
+=====================
+class bubble 
+{
+    Public Static vid main(String args[])
+    {
+        int arr[] = {kunal, ekta, sayli, harsh}
+        int temp;
+        for(int i=0; i<arr.length; i++)
+        {
+            for(int j=0; j<arr.length -i ; j++)
+            {
+                if (arr[j].compareTo(arr[j+1] > 0)
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int i=0; <arr.length ; i++)
+        {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
+
+
+
+
