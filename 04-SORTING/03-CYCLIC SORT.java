46c044ce53d8d94b3c250b76aa24fb67a136b229
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-10-04 23:15:23 +0530

    Update 03-CYCLIC SORT.java

--- 04-SORTING/03-CYCLIC SORT.java
+++ 04-SORTING/03-CYCLIC SORT.java
@@ -22,3 +22,51 @@ ABOVE EG IS WORST CASE SCENARIO
 -> NO OD COMPARISIONS AT END = 5 = ARR.LENGTH = N
 TOTAL SWAPS AND COMPARISIONS = (N-1)+N = 2N-1 = O(N) //AS WE IGNORE CONSTANTS
 
+  STEP 1 : CHECK ITS CORRECT INDEX
+  STEP 2 : SWAP WITH CORRECT INDEX
+  STEP 3 : MOVE THE ith INDEX
+
+  IF ARRAY IS FROM 1 TO N
+
+  public class CyclicSort{
+  public static void main(String args[])
+  {
+    int arr[] = {5,3,2,1,4};
+    swap(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+  static void cyclicsort(int arr[])
+  {
+    while(i < arr.length)
+    {
+      int correctindex = arr[i] - 1;
+      if(arr[i]  != correctindex)
+      {
+        swap(arr, arr[i], correctindex);}
+      else
+      {
+        i++;}
+    }
+  }
+  static void swap(int arr[]; int element, int correctindex)
+  {
+    int temp = arr[element];
+    arr[element]=arr[correctindex];
+    arr[correctindex] = temp.
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
