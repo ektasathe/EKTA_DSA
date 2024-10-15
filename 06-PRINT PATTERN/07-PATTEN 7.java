dcebb63656a765f4229043eb4804dc351e6a7ec3
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-10-15 20:08:58 +0530

    Create 07-PATTEN 7.java

---
+++ 06-PRINT PATTERN/07-PATTEN 7.java
@@ -0,0 +1,28 @@
+   * 
+  * * 
+ * * * 
+* * * * 
+ * * * 
+  * * 
+   * 
+
+class HelloWorld {
+    public static void main(String[] args) {
+       pattern(4);
+    }
+    static void pattern(int n) {
+         for (int row = 0; row < 2 * n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row: row;
+
+            int noOfSpaces = n - totalColsInRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
