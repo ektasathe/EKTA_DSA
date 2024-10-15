27a60690626d342537181bd385512960ee9edfbd
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-10-15 19:57:00 +0530

    Update and rename 03 to 04-PATTERN 4.java

--- 06-PRINT PATTERN/04-PATTERN 4.java
+++ 06-PRINT PATTERN/04-PATTERN 4.java
@@ -1,21 +1,20 @@
-* * * * 
-* * * 
-* * 
-* 
+1 
+1 2 
+1 2 3 
+1 2 3 4 
 
 class HelloWorld {
     public static void main(String[] args) {
        pattern(4);
     }
-
-     static void pattern(int n) {
+    static void pattern(int n) {
         for (int row = 1; row <= n; row++) {
             // for every row, run the col
-            for (int col = 1; col <= n-row+1; col++) {
-                System.out.print("* ");
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
             }
             // when one row is printed, we need to add a newline
             System.out.println();
         }
-     }
+    }
 }
