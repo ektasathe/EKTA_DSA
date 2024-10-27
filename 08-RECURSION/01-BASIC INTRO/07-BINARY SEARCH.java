5747ce6139c39b5b87bdddf8c11685065fa6c721
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-10-27 23:15:25 +0530

    Update 07-BINARY SEARCH.java

--- 08-RECURSION/01-BASIC INTRO/07-BINARY SEARCH.java
+++ 08-RECURSION/01-BASIC INTRO/07-BINARY SEARCH.java
@@ -5,3 +5,41 @@ RECURSION IN BINARY SEARCH
  -------------------------------------------------------------------
    O(1) --> COMPARISION
    O(N/1) --> REDUCE THE SEARCH SPACE BY FACTOR
+
+
+class recursionsearch {
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,7,8,9,11,13,15};
+        int t = 13 ;
+        System.out.println(binsearch(arr ,t,0,arr.length-1));
+    }
+    static int binsearch(int[] arr, int t, int s, int e)
+    {
+        //here the base condition would be when the loop breaks
+        if(s>e){
+            return -1;
+        }
+        int m = s + (e-s)/2;
+        if(arr[m]==t){
+            return arr[m];
+        }
+        if(t<m){
+            //e=m-1;
+           return binsearch(arr, t, s, m-1);
+        }
+        else {
+           return binsearch(arr, t, m+1, e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+ 
