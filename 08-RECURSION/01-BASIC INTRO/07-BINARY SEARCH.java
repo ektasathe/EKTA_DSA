0b72fcb8992a23c3e7a6adf52545b5ee6d485f93
Ekta Sathe <70477615+ektasathe@users.noreply.github.com>
2024-10-27 21:36:57 +0530

    Update 07-BINARY SEARCH.java

--- 08-RECURSION/01-BASIC INTRO/07-BINARY SEARCH.java
+++ 08-RECURSION/01-BASIC INTRO/07-BINARY SEARCH.java
@@ -1,1 +1,7 @@
+RECURSION IN BINARY SEARCH
 
+ -------------------------------------------------------------------
+| F(N) = O(1) + F(N/2)  = CONSTANT TIME + DIVIDE THE ARR INTO HALF) |
+ -------------------------------------------------------------------
+   O(1) --> COMPARISION
+   O(N/1) --> REDUCE THE SEARCH SPACE BY FACTOR
